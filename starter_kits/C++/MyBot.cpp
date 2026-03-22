bd6108ce208b415dfef8b0892b0c149aa572a0a2
DGoblot <90686224+DGoblot@users.noreply.github.com>
2026-03-22 11:27:52 +0100

    Add initial_game_map to precompute stuff

--- starter_kits/C++/MyBot.cpp
+++ starter_kits/C++/MyBot.cpp
@@ -21,6 +21,9 @@ int main(int argc, char* argv[]) {
     // At this point "game" variable is populated with initial map data.
     // This is a good place to do computationally expensive start-up pre-processing.
     // As soon as you call "ready" function below, the 2 second per turn timer will start.
+
+    unique_ptr<GameMap>& initial_game_map = game.game_map;
+
     game.ready("MyCppBot");
 
     log::log("Successfully created bot! My Player ID is " + to_string(game.my_id) + ". Bot rng seed is " + to_string(rng_seed) + ".");
