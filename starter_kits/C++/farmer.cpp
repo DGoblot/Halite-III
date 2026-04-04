33cbb811c5da4cea12474fa88c71956be6b08507
DGoblot <90686224+DGoblot@users.noreply.github.com>
2026-04-04 15:32:46 +0200

    ship now go home normally

--- starter_kits/C++/farmer.cpp
+++ starter_kits/C++/farmer.cpp
@@ -1,5 +1,8 @@
 ﻿#include "farmer.hpp"
 #include "hlt/log.hpp"
+#include <unordered_map>
+
+static std::unordered_map<hlt::EntityId, bool> s_goingHomeStates;
 
 FarmerBT::FarmerBT()
 {
@@ -39,8 +42,19 @@ void FarmerBT::evaluate(hlt::Game* game,
     Context ctx{ game, ship, command };
 
     hlt::log::log("Ship " + std::to_string(ship->id));
+    bool goingHomeFlag = false;
+    auto it = s_goingHomeStates.find(ship->id);
+    if (it != s_goingHomeStates.end()) {
+        goingHomeFlag = it->second;
+    }
 
-    tree->evaluate(ctx);
+    hlt::log::log(std::to_string(goingHomeFlag));
+    if (goingHomeFlag)
+    {
+        goingBackHome(ctx);
+        hlt::log::log("just going home bruh");
+    }
+    else { tree->evaluate(ctx); }
 }
 
 BT_NODE::State FarmerBT::enemyTooClose(Context& ctx)
@@ -147,6 +161,8 @@ BT_NODE::State FarmerBT::worthMoving(Context& ctx)
 
 BT_NODE::State FarmerBT::goingBackHome(Context& ctx)
 {
+    s_goingHomeStates[ctx.ship->id] = true;
+
     hlt::Direction nextDir;
     if(ctx.game->game_map->calculate_distance(ctx.ship->position,  ctx.game->me->shipyard->position) <= 1)
     {
@@ -161,6 +177,13 @@ BT_NODE::State FarmerBT::goingBackHome(Context& ctx)
         ctx.ship->move(nextDir);
 
     hlt::log::log("Home run : " + std::to_string(ctx.ship->position.directional_offset(nextDir).x) + ", " + std::to_string(ctx.ship->position.directional_offset(nextDir).y));
+    if (ctx.game->game_map->calculate_distance(ctx.ship->position, ctx.game->me->shipyard->position) == 0) {
+        s_goingHomeStates[ctx.ship->id] = false;
+        if (ctx.game->me->ships.find(ctx.ship->id) != ctx.game->me->ships.end()) {
+            ctx.game->me->ships[ctx.ship->id]->goingHome = false;
+        }
+    }
+    
     return BT_NODE::State::SUCCESS;
 }
 
@@ -191,7 +214,7 @@ BT_NODE::State FarmerBT::goingToHaliteSpot(Context& ctx)
     {
 		nextDir = ctx.game->game_map->naive_navigate(
 			ctx.ship,
-			*(ctx.game->game_map->find_path(ctx.ship->position, ctx.bestNearbyCell, exitingStructure).end()-2));
+			*(ctx.game->game_map->find_path(ctx.ship->position, ctx.bestNearbyCell, exitingStructure).end()-2));    
     }
     *ctx.command =
         ctx.ship->move(nextDir);
